b1fda7f25d54ba54eae90a4f81d6fd34783857b5
eumcloud <livelch012@naver.com>
2022-05-31 12:22:09 +0000

    mini test - web3js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const web3 = require("../web3");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+let wallets = web3.eth.personal.getAccounts().then(console.log);
+
+router.get("/", (req, res)=>{
+    
+    res.render("index", { wallets: wallets }) 
 });
+
+router.get("/newaccount", (req, res)=>{
+    
+    let address = web3.eth.personal.newAccount('1').then(console.log);
+    res.render("index", {wallets: wallets, address: address})
+})
 
 module.exports = router;
